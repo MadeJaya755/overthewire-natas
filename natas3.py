a4ebc6b79f7a9079477b641aefef18495d67d456
Made Jaya Setiawan <124169901+MadeJaya755@users.noreply.github.com>
2025-09-09 18:48:43 +0700

    Update natas3.py

--- natas3.py
+++ natas3.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 
 # Kredensial Natas 3
 username = 'natas3'
-password = '<password_natas3>'
+password = 'QryZXc2e0zahULdHrtHxzyYkj59kUxLQ'
 url = 'http://natas3.natas.labs.overthewire.org/'
 
 # Request halaman dengan HTTP Basic Auth
